8cf8931fddf0c7483af1955d819efe6fc23aefb4
MATA's Mac <matembebenjamin@gmail.com>
2023-05-03 01:54:34 +0300

    Add html and  css rules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './style.css';
+import iterate from './modules/loader.js';
+// const ulList = document.querySelector('.createdList');
+
+// const list = [
+//   { description: ['wash cups', 'goods for sale', 'clean the house', 'yello yellow'] },
+//   { completed: [false, false, false, false] },
+//   { index: [1, 2, 3, 4] },
+
+// ];
+
+// const iterate = () => {
+//   for (let i = 0; i < list[0].description.length; i += 1) {
+//     ulList.innerHTML += `<li><input type="checkbox"/>${list[0].description[i]}</li>`;
+//   }
+// };
+
+window.addEventListener('load', () => {
+  iterate();
+});
---
+++ src/modules/loader.js
@@ -0,0 +1,16 @@
+const ulList = document.querySelector('.createdList');
+
+const list = [
+  { description: ['wash cups', 'goods for sale', 'clean the house', 'yello yellow'] },
+  { completed: [false, false, false, false] },
+  { index: [1, 2, 3, 4] },
+
+];
+
+const iterate = () => {
+  for (let i = 0; i < list[0].description.length; i += 1) {
+    ulList.innerHTML += `<li><input type="checkbox"/>${list[0].description[i]}</li>`;
+  }
+};
+
+export default iterate;
